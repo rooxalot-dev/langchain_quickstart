83831908ee3f270e8e28f7db1e808a84f65f9424
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-07 17:42:11 -0300

    Implement simple chat prompt template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello Langchain!');
+import { config } from 'dotenv';
+
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { ChatOpenAI } from '@langchain/openai';
+
+config();
+
+const invokeChat = async () => {
+    // Obtains OPENAI_API_KEY from environment variables
+    const chatModel = new ChatOpenAI();
+
+    const prompt = ChatPromptTemplate.fromMessages([
+        ["system", "You are a world class technical documentation writer."],
+        ["user", "{input}"],
+      ]);
+
+    const chain = prompt.pipe(chatModel);
+    const chatResponse = await chain.invoke({ input: 'What is LangSmith?' });
+    console.log('invokeChat', { chatResponse });
+}
+
+invokeChat();
