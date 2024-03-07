450118a2e7555a981dea258c603eb64f83d84358
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-07 17:51:51 -0300

    Add parser to enforce the return of the string result

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { ChatOpenAI } from '@langchain/openai';
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 config();
 
@@ -10,11 +11,15 @@ const invokeChat = async () => {
     const chatModel = new ChatOpenAI();
 
     const prompt = ChatPromptTemplate.fromMessages([
-        ["system", "You are a world class technical documentation writer."],
+        ["system", "You are a world class technical documentation writer with relevant knowledge on technology."],
         ["user", "{input}"],
       ]);
+    const outputParser = new StringOutputParser();
 
-    const chain = prompt.pipe(chatModel);
+    const chain = prompt
+        .pipe(chatModel)
+        .pipe(outputParser);
+
     const chatResponse = await chain.invoke({ input: 'What is LangSmith?' });
     console.log('invokeChat', { chatResponse });
 }
