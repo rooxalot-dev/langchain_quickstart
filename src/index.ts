aae0a1eb76f5abc982925143715358283fde2a39
Rodrigo Martins de Azevedo <rooxalot@gmail.com>
2024-03-12 21:28:18 -0300

    Implement simple retrieval chain with In-memory vectorstore and OpenAI Embeddings

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { config } from 'dotenv';
 
-import { ChatOpenAI } from '@langchain/openai';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-
-import { OpenAIEmbeddings } from "@langchain/openai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 
 config();
 
@@ -21,6 +21,7 @@ const invokeChat = async () => {
 
   const splitter = new RecursiveCharacterTextSplitter();
   const splitDocs = await splitter.splitDocuments(docs);
+
   console.log('Loaded context splitted doc', {
     splits: splitDocs.length,
     contentLength: splitDocs[0].pageContent.length,
@@ -31,19 +32,32 @@ const invokeChat = async () => {
     splitDocs,
     embeddings
   );
+  const vectorStoreRetriever = vectorStore.asRetriever();
 
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["system", "You are a world class technical documentation writer with relevant knowledge on technology."],
-    ["user", "{input}"],
-  ]);
-  const outputParser = new StringOutputParser();
+  const prompt = ChatPromptTemplate.fromTemplate(`
+    Answer the following question basend only on the provided context:
 
-  const chain = prompt
-    .pipe(chatModel)
-    .pipe(outputParser);
+    <context>
+    {context}
+    </context>
 
-  //const chatResponse = await chain.invoke({ input: 'What is LangSmith?' });
-  //console.log('invokeChat', { chatResponse });
+    Question: {input}
+  `);
+
+  const documentChain = await createStuffDocumentsChain({
+    llm: chatModel,
+    prompt,
+    outputParser: new StringOutputParser(),
+  });
+
+  const retrievalChain = await createRetrievalChain({
+    combineDocsChain: documentChain,
+    retriever: vectorStoreRetriever,
+  });
+
+  const answerResponse = await retrievalChain.invoke({ input: 'what is LangSmith?' });
+
+  console.log('invokeChat -> Response', { answerResponse });
 }
 
 invokeChat();
