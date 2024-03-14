a3c34ead23169e29a00a711560beb174c5a4ad6d
Rodrigo <rooxalot@gmail.com>
2024-03-13 23:15:36 -0300

    add mock history aware convesation with the retrieved data from the vector store

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { config } from 'dotenv';
 
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
-import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { ChatPromptTemplate, MessagesPlaceholder } from '@langchain/core/prompts';
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 
 config();
 
@@ -35,11 +38,13 @@ const invokeChat = async () => {
   const vectorStoreRetriever = vectorStore.asRetriever();
 
   const prompt = ChatPromptTemplate.fromTemplate(`
-    Answer the following question basend only on the provided context:
+    Answer the following question based only on the provided context:
 
     <context>
     {context}
     </context>
+
+    You don't need to explain that the answer that you'll give is based on the context.
 
     Question: {input}
   `);
@@ -57,7 +62,36 @@ const invokeChat = async () => {
 
   const answerResponse = await retrievalChain.invoke({ input: 'what is LangSmith?' });
 
-  console.log('invokeChat -> Response', { answerResponse });
+  // Conversational Chain
+
+  const historyAwarePrompt = ChatPromptTemplate.fromMessages([
+    new MessagesPlaceholder("chat_history"),
+    ["user", "{input}"],
+    [
+      "user",
+      "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation",
+    ],
+  ]);
+
+  const historyAwareRetrieverChain = await createHistoryAwareRetriever({
+    llm: chatModel,
+    retriever: vectorStoreRetriever,
+    rephrasePrompt: historyAwarePrompt,
+  });
+
+  const mockChatHistory = [
+    new HumanMessage("Can LangSmith help test my LLM applications?"),
+    new AIMessage("Yes!"),
+  ];
+
+  const historyAwareAnswer = await historyAwareRetrieverChain.invoke({
+    chat_history: mockChatHistory,
+    input: 'Tell me how!'
+  });
+
+  console.log('invokeChat -> Response', { historyAwareAnswer });
+
+
 }
 
 invokeChat();
